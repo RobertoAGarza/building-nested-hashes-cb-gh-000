eee4229a3a1731466cb986846704673f1079e9b1
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 15:31:38 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        nmae: "Lady Capulet",
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
